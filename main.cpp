17c44b50e9b6af9971df136f1dae0881acb4f67c
NC-KOREA\silblue04 <bluevill04@gmail.com>
2021-11-04 13:40:26 +0900

    DP.protected constructor

--- main.cpp
+++ main.cpp
@@ -9,3 +9,46 @@ int main()
     cout << "Hello!" << endl;
     return 0;
 }
+
+
+
+void ProtectedConstructor()
+{
+    
+}
+
+class InitTest
+{
+private :
+    static int si;
+    const int ci;
+
+public :
+    InitTest(int ci)
+    : ci(ci)
+    {
+
+    }
+
+public :
+    void Destroy()
+    {
+        delete this;
+    }
+
+protected :
+    ~InitTest() 
+    {
+        // 스택에 객체를 만들 수 없다 대신 힙에서는 가능 (new)
+        // delete 직접 호출 불가
+        // 멤버 함수를 통해서 삭제 해야함
+    }
+};
+int InitTest::si = 0;
+
+
+
+void Upcasting()
+{
+    
+}
