b0af40d447aeeaecdc7f4d2ad37f0b3ce33aea7e
silblue04 <silblue04@gmail.com>
2021-11-11 23:06:56 +0900

    // Section 3: 재귀적 포함 - menu event #1

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <list>
 
 using namespace std;
 
@@ -11,6 +12,7 @@ int main()
 }
 
 
+// Section 1: Warning Up
 
 void ProtectedConstructor()
 {
@@ -57,5 +59,73 @@ void Upcasting()
 
 void Coupling()
 {
-    
+
 }
+
+
+
+// Section 2: 공통성과 가변성의 분리
+
+// Section 3: 재귀적 포함
+
+class BaseMenu
+{
+private :
+    string _title;
+    int _id;
+
+public :
+    BaseMenu(string title, int id) : _title(title), _id(id) {}
+    string GetTitme() const { return _title; }
+
+    virtual BaseMenu* GetSubMenu(int idx) = 0;
+    virtual void Command() = 0;
+};
+
+class PopupMenu : public BaseMenu
+{
+private :
+    vector<BaseMenu*> _menuList;
+
+public :
+    PopupMenu(string title, int id) : BaseMenu(title, id) {}
+    void AddMenu(BaseMenu* menu) { _menuList.push_back(menu); }
+
+    virtual void Commonad()
+    {
+        // Draw MenuList
+    }
+};
+
+struct IMenuListener
+{
+    virtual void DoCommand() = 0;
+    virtual ~IMenuListener() {}
+};
+
+class MenuItem : public BaseMenu
+{
+private :
+    list<IMenuListener*> _listenerList;
+
+public :
+    MenuItem(string title, int id) : BaseMenu(title, id) {}
+    void AddListener(IMenuListener* listener) { _listenerList.push_back(listener); }
+
+    virtual void Commonad()
+    {
+        for(auto* listener : _listenerList)
+        {
+            listener->DoCommand();
+        }
+    }
+};
+
+class Dialog : public IMenuListener
+{
+public :
+    virtual void DoCommand()
+    {
+        // Dialog method
+    }
+};
