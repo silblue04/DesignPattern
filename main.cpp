485493e3c0c63b2ae7a11ff1c36aa2770f86743e
silblue04 <silblue04@gmail.com>
2021-11-14 14:30:22 +0900

    // Section 5: 통보, 열거, 방문 - Observer 1

--- main.cpp
+++ main.cpp
@@ -185,14 +185,16 @@ int mainOfSection3 ()
 }
 
 
+
 // Section 4: 간접층의 원리 - Adapter
 
-// list를 stack 처럼 보이도록 변경
+// list를 stack 처럼 보이도록 변경 => Stack Adapter!!
 template<typename T> class Stack //: public list<T>
 {
 private :
     list<T> _list;
     // 이것또한 리스트를 쓸 건지 벡터를 쓴건지 템플릿 받아서 처리 가능
+    // template<typename T, typename C = deque<T> > class Stack
 
 public :
     void push(const T& a) { _list.pushback(a); }//{ list<T>::pushback(a); }
@@ -210,4 +212,138 @@ int mainOfSection4()
     // list<T> 를 상속받지말고 포함 이럼 이제 error!
 
     std::cout << s.top() << std::endl;
+}
+
+
+// Section 4: 간접층의 원리 - Proxy
+
+// Calc Proxy : 원격지의 서버를 대신 하는 클래스
+class Calc
+{
+    int server;
+public :
+    Calc() {} //{ server = ec_find_server(3333); }
+
+    int Add(int a, int b)//{ return ec_send_server(server, 1, a, b); }
+    {
+        // 서버 1 명령어 실행
+    }
+    int Sub(int a, int b)//{ return ec_send_server(server, 2, a, b); }
+    {
+        // 서버 2 명령어 실행
+    }
+    //자주 사용되는 요청에 대한 캐시 기능도 추가 가능
+
+    // Proxy <-> Stub
+    // Proxy는 함수 호출을 명령 코드로 변경해서 서버에 전달
+    // Stub은 명령 코드를 다시 함수 호출로 변경
+    // Proxy는 stub과 통신
+};
+
+int mainOfProxy()
+{
+    Calc* c = new Calc();
+    c->Add(10, 8); // ec_send_server(server, 1, 10, 8);
+    c->Sub(10, 8); // ec_send_server(server, 2, 10, 8);
+    // 명령 1, 2 번 신경 xxx                      
+}
+
+// Section 4: 간접층의 원리 - facade
+// 서브 시스템을 합성하는 다수의 객체들의 인터페이스 집합에 관해
+// 일관된 하나의 인터페이스를 제공할 수 있게 한다.
+// Facade는 서브시스템을 사용하기 위한 포괄적 개념의 인터페이스를 제공한다
+
+
+// Section 4: 간접층의 원리 - bridge & PIMPL
+// Pointer to IMPlementation 의 장점
+// 컴파일 속도를 향상 시킨다.
+// 완벽한 정보 은닉이 가능하다 - 헤더파일을 감출 수 있다.
+
+// 인터페이스의 변경        Adapter
+// 대행자                   Proxy
+// 서브시스템의 복잡한 과정을 감추기 위해   Facade
+// Update를 독립적으로 수행하기 위해        Bridge
+
+
+
+// Section 5: 통보, 열거, 방문 - Observer
+
+struct IGraph
+{
+    virtual void Update(int) = 0;
+    virtual ~IGraph() {}
+};
+
+// // 관찰의 대상을 서브젝트라고 부름
+// class Subject
+// {
+
+// };
+
+class Table
+{
+    vector<IGraph*> _graphList;
+    int data;
+public :
+    void Attach(IGraph* graph)
+    {
+        _graphList.push_back(graph);
+    }
+    void  Detach(IGraph* graph)
+    {
+        //
+    }
+    void SetData(int d)
+    {
+        data = d;
+        for (auto graph : _graphList)
+        {
+            graph->Update(data);
+        }
+    }
+};
+
+class PieGraph : public IGraph
+{
+public :
+    virtual void Update(int n)
+    {
+        Draw(n);
+    }
+    void Draw(int n)
+    {
+        for(int i = 0; i < n; ++i)
+        {
+            // Draw
+        }
+    }
+};
+
+class BareGraph : public IGraph
+{
+public :
+    virtual void Update(int n)
+    {
+        Draw(n);
+    }
+    void Draw(int n)
+    {
+        for(int i = 0; i < n; ++i)
+        {
+            // Draw
+        }
+    }
+};
+
+int mainOfObserver()
+{
+    BareGraph bg;
+    PieGraph pg;
+
+    Table t;
+    t.Attach(&bg);
+    t.Attach(&pg);
+
+    // 입력받고
+    t.SetData(20);
 }
